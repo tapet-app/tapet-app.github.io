b8f5e1c3d663315d4e63b43dec78ee7a98bbc812
Adrian Aisemberg <adrianaisemberg@gmail.com>
2024-03-15 21:58:02 -0400

    web

--- tapet/src/App.js
+++ tapet/src/App.js
@@ -6,18 +6,24 @@ function App() {
   return (
     <div className="App">
       <Container>
+        <Col>
+          <Row>
+            <Image src={require('./img/logo.png')} id='logo'/>
+          </Row>
 
-        <Image src={require('./logo.png')} height='60' id='logo'/>
+          <Row>
+            <Image src={require('./img/patterns_01.png')} id='patterns'/>
+          </Row>
 
-        <Row id='google-play'>
-          <a href='https://play.google.com/store/apps/details?id=com.sharpregion.tapet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-            <img
-              height="80"
-              alt='Get it on Google Play'
-              src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
-          </a>
-        </Row>
-
+          <Row id='google-play'>
+            <a href='https://play.google.com/store/apps/details?id=com.sharpregion.tapet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+              <img
+                height="80"
+                alt='Get it on Google Play'
+                src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+            </a>
+          </Row>
+        </Col>
       </Container>
     </div>
   );
